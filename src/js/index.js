d53673b7e03eea8c6fdfa043223a2c30f9c4fc12
Clement <clement07052005@gmail.com>
2022-02-15 14:58:29 +0100

    animations v1

--- src/js/index.js
+++ src/js/index.js
@@ -9,12 +9,16 @@ gsap
   .timeline({
     scrollTrigger: {
       trigger: ".content",
-      start: "center center",
-      end: "bottom top",
+      start: "-20% top",
+      end: "-20% top",
       // markers: true,
-      scrub: true,
-      pin: true,
+      scrub: false,
+      // pin: true,
+      anticipatePin: 1,
     },
+  })
+  .to(".scroll", {
+    opacity: 0,
   })
   .to(".arg1", {
     opacity: 1,
@@ -30,4 +34,28 @@ gsap
     opacity: 1,
   }).to("html", {
       "--args-opacity": 1,
+  });
+  
+  
+  gsap.timeline({
+    scrollTrigger: {
+      trigger: ".why",
+      start: "top top",
+      end: "top top",
+      markers: true,
+      scrub: false,
+
+    },
+  }).to(".languages", {
+    y: -100,
+    opacity: 1,
+  }).to(".auto-translate", {
+    y: -100,
+    opacity: 1,
+  }).to(".no-pollute", {
+    y: -100,
+    opacity: 1,
+  }).to(".premium", {
+    y: -100,
+    opacity: 1,
   })
