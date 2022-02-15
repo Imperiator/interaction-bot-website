af40117204d6f143b50e4fd972f958af96a3551f
Clement <clement07052005@gmail.com>
2022-02-15 17:16:54 +0100

    mooved list to main page and changed animation

--- src/js/index.js
+++ src/js/index.js
@@ -4,58 +4,26 @@ let stats = document.querySelector(".ul-stats");
 login.addEventListener("click", function () {
   window.location.href = "/login.html";
 });
-
-gsap
-  .timeline({
-    scrollTrigger: {
-      trigger: ".content",
-      start: "-20% top",
-      end: "-20% top",
-      // markers: true,
-      scrub: false,
-      // pin: true,
-      anticipatePin: 1,
-    },
-  })
-  .to(".scroll", {
-    opacity: 0,
-  })
-  .to(".arg1", {
-    opacity: 1,
-  }).to(".arg2", {
-    opacity: 1,
-  }).to(".arg3", {
-    opacity: 1,
-  }).to(".arg4", {
-    opacity: 1,
-  }).to(".arg5", {
-    opacity: 1,
-  }).to(".arg6", {
-    opacity: 1,
-  }).to("html", {
-      "--args-opacity": 1,
-  });
-  
   
   gsap.timeline({
     scrollTrigger: {
-      trigger: ".why",
-      start: "top top",
-      end: "top top",
+      trigger: ".content",
+      start: "top 20%",
+      end: "top -25%",
       markers: true,
-      scrub: false,
+      scrub: 1,
 
     },
-  }).to(".languages", {
-    y: -100,
-    opacity: 1,
-  }).to(".auto-translate", {
-    y: -100,
-    opacity: 1,
-  }).to(".no-pollute", {
-    y: -100,
-    opacity: 1,
-  }).to(".premium", {
-    y: -100,
-    opacity: 1,
+  }).from(".languages", {
+    y: 100,
+    opacity: 0,
+  }).from(".auto-translate", {
+    y: 100,
+    opacity: 0,
+  }).from(".no-pollute", {
+    y: 100,
+    opacity: 0,
+  }).from(".premium", {
+    y: 100,
+    opacity: 0,
   })
